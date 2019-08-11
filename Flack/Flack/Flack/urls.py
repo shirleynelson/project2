bc208d0a68bf79c05151ef948414c52c6bf2e625
shirleynelson <52511947+shirleynelson@users.noreply.github.com>
2019-08-11 09:18:52 -0400

    Add Django Rest Framework settings and custom api settings.

--- Flack/Flack/Flack/urls.py
+++ Flack/Flack/Flack/urls.py
@@ -50,3 +50,55 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^', include(router.urls)),
 ]
+"""
+Definition of urls for Flack.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('app/',include('app.urls'),
+    path('app/api/',include('app.api.urls')),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^', include(router.urls)),
+]
